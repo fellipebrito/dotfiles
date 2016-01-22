1e10c8a8063dc720121657a52b739c738b370c95
Fellipe Brito <fellipe@devlandia.net>
2016-01-22 09:04:41 +0000

    New Vagrant configuraiton

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,56 +1,24 @@
-# Avoid Mysql Installation Prompt
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+# remove ruby
+sudo apt-get remove ruby
 
-# Language Settings
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen en_US.UTF-8
-
-# Add Repository
-sudo apt-get -y -q install python-software-properties
-sudo add-apt-repository ppa:pi-rho/dev
-sudo apt-get update
-
-# install basic unix packages
+# install essential apps
 sudo apt-get install -y -q \
-  bash-completion \
   build-essential \
-  curl \
   git \
-  libmysqlclient-dev \
-  libcurl3-dev \
-  libxslt-dev \
-  libxml2-dev \
-  mysql-server \
-  nodejs \
-  nginx \
   rake \
-  screen \
-  tree \
-  tmux \
-  vim \
-  wget
+  tree
 
+# install ruby using rvm
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
-# clone dotfiles
-su -c 'git clone https://github.com/fellipebrito/dotfiles.git ~/.dotfiles && cd ~/.dotfiles && rake install;' vagrant
+# creates the basic directory
+mkdir ~/www
 
-# remove vagrant default ruby and install rvm
-su -c 'sudo rm -rf /opt/vagrant_ruby/ && \curl -sSL https://get.rvm.io | bash -s stable --rails --ruby=2.1.1;' vagrant
+# download and install dotfiles
+git clone git@github.com:fellipebrito/dotfiles.git ~/.dotfiles && cd ~/.dotfiles && rake install
+ln -s bin/ /home/vagrant/bin
+source ~/.bashrc
 
-# install vundle
-# TODO: BundleInstall must run in a screen
-su -c 'mkdir -p ~/.vim/bundle' vagrant
-su -c 'git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle && vim +BundleInstall +qall;' vagrant
-su -c 'clear' vagrant
-
-# configure app env
-su -c 'sudo mkdir /var/www && sudo chmod 777 -R /var/www;' vagrant
-su -c 'ln -s /var/www ~/www;' vagrant
-
-# pssh / portly
-gem install pssh
-
-exit
+# install vim plugins
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle && vim +BundleInstall +qall
