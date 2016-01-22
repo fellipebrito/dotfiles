912b8fe1e04ae957ff29dc053a40c94b72c861a8
Fellipe Brito <fellipe@devlandia.net>
2016-01-22 10:16:09 +0000

    wip vagrant

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -15,15 +15,15 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 # creates the basic directory
 mkdir -p /home/vagrant/www
 
-download and install dotfiles
+# download and install dotfiles
 ssh-keyscan github.com >> /home/vagrant/.ssh/known_hosts
 rm -rf /home/vagrant/dotfiles
-su -c 'git clone git@github.com:fellipebrito/dotfiles.git /home/vagrant/dotfiles && cd /home/vagrant/dotfiles && rake install' vagrant
+git clone git@github.com:fellipebrito/dotfiles.git /home/vagrant/dotfiles && cd /home/vagrant/dotfiles && rake install
 rm /home/vagrant/bin
 ln -s /home/vagrant/dotfiles/bin/ /home/vagrant/bin
-su -c 'source /home/vagrant/.bashrc'
+source /home/vagrant/.bashrc
 
 # install vim plugins
 rm -rf /home/vagrant/.vim/bundle/vundle
 git clone https://github.com/gmarik/vundle.git /home/vagrant/.vim/bundle/vundle
-su -c 'vim +PluginInstall +qall' vagrant
+vim +PluginInstall +qall
